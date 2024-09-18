ed252d5af609f50787c1a410ce64b536e5d404bb
raviteja011998 <ravitejakandagatla1998@gmail.com>
2024-09-18 22:13:38 +0530

    adding files to github repo

---
+++ andornot.py
@@ -0,0 +1,9 @@
+x,y = True, False
+if (x or y) == True:
+    print("Scum")
+elif (x and y) == False:
+    print("Scumbag")
+elif (x != y) == True:
+    print("Skzid")
+else:
+    print("Value is not given")            
---
+++ falseandtrue.py
@@ -0,0 +1,9 @@
+x = 1
+y = 2
+
+if False == (1 < 2):
+    print("False")
+elif True == (2 > 1):
+    print(True)
+else:
+    print("Carbon Copy")
